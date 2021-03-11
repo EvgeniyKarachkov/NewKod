67a36a74011f712721ba90ea380af50adb1a116d
EvgeniyKarachkov <cazinak341@gmail.com>
2021-03-12 00:02:59 +0300

    Added protocols for Linked List implementation

---
+++ 03082021_calculator/03082021_calculator/Node.swift
@@ -0,0 +1,15 @@
+//
+//  Node.swift
+//  03082021_calculator
+//
+//  Created by Zodino BLR on 3/11/21.
+//  Copyright © 2021 Zodino BLR. All rights reserved.
+//
+
+import Foundation
+
+protocol Node {
+    var value: Int {get set}
+    var next: Node {get set}
+    
+}
